6f8d50136d868f6de4223fdf898ff43646fed7bd
Justin Anderson <jandersonis@gmail.com>
2010-06-05 04:39:20 -0700

    updated tns table

--- qtodo.rb
+++ qtodo.rb
@@ -25,17 +25,26 @@ ActiveRecord::Base.establish_connection(
 begin
   ActiveRecord::Schema.define do
     create_table :tns do |t|
-      t.string :tn
-      t.string :target
-      t.string :request_type
-      t.string :device_type
       t.string :region
       t.string :date_requested
-      t.string :priority
       t.string :due_date
-      t.string :capture_type
-      t.string :brand
+      t.string :bl
       t.string :requestor
+      t.string :request_type
+      t.string :priority
+      t.string :tn
+      t.string :brand
+      t.string :device_type
+      t.string :remote
+      t.string :target
+      t.string :published_ids
+      t.string :exec
+      t.string :capture_type
+      t.string :split_ids
+      t.string :requestor_notified
+      t.string :comments
+      t.string :tn_location
+      t.string :tn_status
     end
   end
 rescue ActiveRecord::StatementInvalid
@@ -66,30 +75,44 @@ post '/upload' do
   array_of_hashes.each do |tn|
     existing_tn = Tn.find(:first, :conditions => { :tn => tn["TN"] })
     unless existing_tn
-      Tn.create(:tn => tn["TN"],
-                :target => tn["Target #"],
-                :request_type => tn["Request Type"],
-                :device_type => tn["Device"],
-                :region => tn["Region"],
+      Tn.create(:region => tn["Region"],
                 :date_requested => tn["Date Requested"],
-                :priority => tn["Priority"],
                 :due_date => tn["Due Date"],
-                :capture_type => tn["Capture Type"],
+                :bl => tn["BL"],
+                :requestor => tn["Requestor"],
+                :request_type => tn["Request Type"],
+                :priority => tn["Priority"],
+                :tn => tn["TN"],
                 :brand => tn["Brand"],
-                :requestor => tn["Requestor"])
+                :device_type => tn["Device"],
+                :remote => tn["Remote #"],
+                :target => tn["Target #"],
+                :published_ids => tn["Published ID(s)"],
+                :exec => tn["Exec"],
+                :capture_type => tn["Capture Type"],
+                :split_ids => tn["Split ID(s)"],
+                :requestor_notified => tn["Requestor Notified (Y/N)"],
+                :comments => tn["Comment(s)"])
     else
       Tn.update(existing_tn.id, {
-                :tn => tn["TN"],
-                :target => tn["Target #"],
-                :request_type => tn["Request Type"],
-                :device_type => tn["Device"],
                 :region => tn["Region"],
                 :date_requested => tn["Date Requested"],
-                :priority => tn["Priority"],
                 :due_date => tn["Due Date"],
-                :capture_type => tn["Capture Type"],
+                :bl => tn["BL"],
+                :requestor => tn["Requestor"],
+                :request_type => tn["Request Type"],
+                :priority => tn["Priority"],
+                :tn => tn["TN"],
                 :brand => tn["Brand"],
-                :requestor => tn["Requestor"]})
+                :device_type => tn["Device"],
+                :remote => tn["Remote #"],
+                :target => tn["Target #"],
+                :published_ids => tn["Published ID(s)"],
+                :exec => tn["Exec"],
+                :capture_type => tn["Capture Type"],
+                :split_ids => tn["Split ID(s)"],
+                :requestor_notified => tn["Requestor Notified (Y/N)"],
+                :comments => tn["Comment(s)"]})
     end
   end
   redirect '/'
